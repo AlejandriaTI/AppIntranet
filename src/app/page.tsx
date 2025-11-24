82e14bacade0a71174d05b2b6b210be23ff81435
Calebcc <calebcondor553@gmail.com>
2025-11-24 12:20:20 -0500

    hazaña: Implementar una página inicial con pantalla de presentación y visualización de inicio de sesión condicional según las preferencias del usuario.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,40 @@
-"use client"; // Asegúrate de que este componente se ejecute solo en el cliente
+"use client";
 
 import { useEffect, useState } from "react";
-import Splash from "@/components/splash"; // Importa tu Splash Screen
-import LoginPage from "@/components/auth/login-form"; // Importa la página de login
+import Splash from "@/components/splash";
+import LoginPage from "@/components/auth/login-form";
+import { Preferences } from "@capacitor/preferences";
 
 export default function Home() {
   const [showLogin, setShowLogin] = useState(false);
 
   useEffect(() => {
-    // Mostrar Splash durante 3 segundos
-    const timer = setTimeout(() => {
-      setShowLogin(true); // Después de 3 segundos, mostramos el Login
-    }, 3000); // Ajusta este tiempo a tu gusto
+    const init = async () => {
+      // Revisar si ya vio el Splash antes
+      const { value } = await Preferences.get({ key: "hasSeenSplash" });
 
-    return () => clearTimeout(timer); // Limpiar el temporizador cuando se desmonta el componente
+      if (value === "true") {
+        // Ya vio el splash → Ir directo al login
+        setShowLogin(true);
+        return;
+      }
+
+      // Primera vez → mostrar splash y luego login
+      const timer = setTimeout(async () => {
+        setShowLogin(true);
+
+        // Guardar bandera para NO volver a mostrar Splash
+        await Preferences.set({
+          key: "hasSeenSplash",
+          value: "true",
+        });
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    };
+
+    init();
   }, []);
 
-  return (
-    <>
-      {!showLogin ? (
-        <Splash /> // Mostrar Splash Screen mientras se espera
-      ) : (
-        <LoginPage /> // Mostrar Login después de 3 segundos
-      )}
-    </>
-  );
+  return <>{!showLogin ? <Splash /> : <LoginPage />}</>;
 }
